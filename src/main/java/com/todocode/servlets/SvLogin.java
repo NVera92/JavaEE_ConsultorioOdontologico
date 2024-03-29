f88cdf29f8903766b3aa38896016f7e09c1fd955
Nicolas Vera <nive7792@gmail.com>
2024-03-29 13:18:40 -0300

    Fix: Redirect page error solved

--- src/main/java/com/todocode/servlets/SvLogin.java
+++ src/main/java/com/todocode/servlets/SvLogin.java
@@ -23,7 +23,7 @@ public class SvLogin extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+       
     }
 
     @Override
